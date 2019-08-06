ec2e2986b4e74c24148d7b2d85975289db69694a
WheelerLee <490386516@qq.com>
2019-08-06 13:27:04 +0800

    1.0.1

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const add = (x, y) => {
-  return x + y;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Created by WheelerLee on 2019-08-06 11:31.
+ * Contacts
+ * Copyright 2019 https://github.com/WheelerLee
+ */
+var add = function (x, y) {
+    return x + y;
 };
-
-export default add;
+exports.default = add;
